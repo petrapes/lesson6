56ddd706acceaa57f29963626de37ffcad6426ea
Petra Peskova <petra.peskova@centrum.cz>
2020-02-27 18:23:29 +0100

    myFirstCommit

--- src/com/engeto/Main.java
+++ src/com/engeto/Main.java
@@ -1,8 +1,17 @@
 package com.engeto;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("What is your name?");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Here is the story: \n" +
+                "Once upon a time there was " + name + ", who was teacher" + ". \n" +
+                "On the way to work, " + name + " reflected on life. \n" +
+                "Perhaps " + name + " will not be teacher" + " forever.");
     }
 }
