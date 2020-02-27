ef3ee1ccb9172438fe3b3f64c954474414d28a6a
Petra Peskova <petra.peskova@centrum.cz>
2020-02-27 18:24:57 +0100

    mySecondCommit

--- src/com/engeto/Main.java
+++ src/com/engeto/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner scanner = new Scanner(System.in);
         System.out.println("What is your name?");
 
         String name = scanner.nextLine();
